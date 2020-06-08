8971e69c8c765a8d8b4651085cd6986fd8a3f874
Andrey Govorovskiy <a.govorovskiy@corp.mail.ru>
2020-06-08 20:33:07 +0300

    fix colors

--- app/src/main/java/com/example/myapplication/ViewExt.kt
+++ app/src/main/java/com/example/myapplication/ViewExt.kt
@@ -23,7 +23,7 @@ fun bindColors(black: Pair<ImageView, Long>,
                blue: Pair<ImageView, Long>,
                yellow: Pair<ImageView, Long>) {
     black.first.background = CircleDrawable(Color.BLACK, black.second)
-    blue.first.background = CircleDrawable(Color.BLUE, blue.second)
-    red.first.background = CircleDrawable(Color.RED, red.second)
-    yellow.first.background = CircleDrawable(Color.YELLOW, yellow.second)
+    blue.first.background = CircleDrawable(Color.parseColor("#08088A"), blue.second)
+    red.first.background = CircleDrawable(Color.parseColor("#8A0808"), red.second)
+    yellow.first.background = CircleDrawable(Color.parseColor("#F4F558"), yellow.second)
 }
